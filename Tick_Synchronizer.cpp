77982a79b892e0f2009e522894ffd7e6cadea374
Kieran Finger <kieran.finger@gmail.com>
2026-08-23 17:09:46 -0500

    Tick Synchronizer Init

---
+++ Tick_Synchronizer.cpp
@@ -0,0 +1,15 @@
+struct SpotTick {
+    long long timestamp;
+    double price;
+};
+
+struct OptionTick {
+    long long timestamp;
+    double price;
+};
+
+class TickSynchronizer {
+private:
+    
+public:
+};
